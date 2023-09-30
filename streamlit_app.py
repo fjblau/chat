1857e6e989fc71fde34690bb5c8c7cd8c6004e2a
Frank Blau <fjblau@gmail.com>
2023-09-30 16:18:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from langchain.llms import OpenAI
 
 st.title('🦜🔗 Quickstart App')
 
-openai_api_key = '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'
+openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
 def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
